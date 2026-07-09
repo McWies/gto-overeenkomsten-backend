a7560a68e171d8eecd35caa4b89f288b65a69c94
McWies <wies@gtonederland.nl>
2026-07-09 15:07:34 +0200

    Add artikelen parameter to function call

--- app.py
+++ app.py
@@ -159,6 +159,7 @@ def generate():
             handtekendatum_nl=data["handtekendatum_nl"],
             tarieven_per_monteur=tarieven,
             opdrachtomschrijving=data["opdrachtomschrijving"],
+            artikelen=data.get("artikelen", {}),
             output_zip_path=str(zip_path),
         )
 
